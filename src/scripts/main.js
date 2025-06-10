23e38d63537cee62f244a564eab853e8a94f3a86
Orest Vaheman <ovaheman@gmail.com>
2025-06-10 21:59:29 +0300

    fix string

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,7 @@ function waitFor(element, eventName) {
   return new Promise((resolve, reject) => {
     element.addEventListener(eventName, (act) => {
       if (act) {
-        resolve(`It was ${eventName}
-          on the element: ${element.nodeName}, id: ${element.id}.`);
+        resolve(`It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`);
       }
     });
   });
